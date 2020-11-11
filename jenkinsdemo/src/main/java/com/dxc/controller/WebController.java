9b192a654000b9fc7a7f58446e23e86467e87a12
lakshmichandrika <chandrikapasupuleti4b2@gmail.com>
2020-11-10 21:50:04 -0800

    first commit

---
+++ jenkinsdemo/src/main/java/com/dxc/controller/WebController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.dxc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lpasupuleti
+ *
+ */
+@RestController
+public class WebController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "HEllo";
+	}
+
+}
